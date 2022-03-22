eb8726c9b552d41b72b8006d34298a9af43c3283
Mike Samuels <msamuels59@gmail.com>
2022-03-22 17:28:00 -0400

    fixed secret key leak

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k7(j1f9d2(3uvner!i_vb70m7j6a=@_f$xsckvg@+e&xaa+mcy'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
