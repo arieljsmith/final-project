32758b02ab863c670f4ad6b591dc40fc4adaff96
Mike Samuels <msamuels59@gmail.com>
2022-04-01 18:26:51 -0400

    superuser creation possible

--- backend/restaurant/models.py
+++ backend/restaurant/models.py
@@ -2,17 +2,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("Users must have an email address")
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
-
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
-
         return user
+
+    def create_superuser(self, email, password, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(email, password, **extra_fields)
+    
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
