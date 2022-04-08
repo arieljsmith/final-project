19b6b5e26c11bbf508b3e5b4905d63df5aad2835
Mike Samuels <msamuels59@gmail.com>
2022-04-08 18:00:37 -0400

    removed /

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    'https://restaurant-recommendations.netlify.app/'
+    'https://restaurant-recommendations.netlify.app'
 ]
 
 # WEBPACK_LOADER = {
