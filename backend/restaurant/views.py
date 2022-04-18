b902fc4a8dee3a124dbed9b326220fe42ed0d000
Mike Samuels <msamuels59@gmail.com>
2022-04-18 18:56:21 -0400

    added token login and refresh views

--- backend/restaurant/views.py
+++ backend/restaurant/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import RestaurantSerializer, CitySerializer, UserSerializer
+from .serializers import RestaurantSerializer, CitySerializer, UserSerializer, TokenSerializer
 from .models import Restaurant, City, UserAccount
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 class RestaurantView(viewsets.ModelViewSet):
     serializer_class = RestaurantSerializer
@@ -14,3 +17,7 @@ class CityView(viewsets.ModelViewSet):
 class UserView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = UserAccount.objects.all()
+
+class TokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = TokenSerializer
