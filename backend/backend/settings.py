f27447b4a3952c077c7cbb419480da14c912a832
apotter303 <63034943+apotter303@users.noreply.github.com>
2022-04-20 16:33:25 -0500

    Change token settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -162,7 +162,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        # 'rest_framework.permissions.AllowAny',
     ]
 }
 
@@ -170,7 +169,7 @@ from datetime import timedelta
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
