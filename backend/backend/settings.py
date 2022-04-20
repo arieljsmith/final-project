cf0a11d69149a15b6a2b9bfe727f81fed6098e6a
apotter303 <63034943+apotter303@users.noreply.github.com>
2022-04-19 23:30:22 -0500

    add logout

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'https://undefined-rest-api.herokuapp.com/',
-    '127.0.0.1'
+    '127.0.0.1',
+    'localhost'
     ]
 
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'restaurant',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -134,8 +136,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
     'https://restaurant-recommendations.netlify.app'
 ]
 
@@ -157,18 +162,29 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.permissions.AllowAny',
     ]
 }
 
 from datetime import timedelta
 
-SIMPLEJWT = {
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
-    "ROTATE_REFRESH_TOKENS": True,
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('JWT'),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
 }
 
-
-# Configure Django App for Heroku
-import django_on_heroku
-django_on_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+# # Configure Django App for Heroku
+# import django_on_heroku
+# django_on_heroku.settings(locals())
+# del DATABASES['default']['OPTIONS']['sslmode']
